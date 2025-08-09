132a571aad13e8341d27b08ebc4494e5d02969a5
Pete Simard <petesimard56@gmail.com>
2025-08-09 18:22:20 +0700

    flatpak update

--- release_flatpak.sh
+++ release_flatpak.sh
@@ -1,1 +1,1 @@
-sudo flatpak-builder --install --force-clean /home/pete/work/stt/builddir /home/pete/work/stt/data/com.petesimard.pineapplewriter.yaml
+sudo flatpak-builder --install --force-clean /home/pete/work/stt/builddir /home/pete/work/stt/data/io.github.petesimard.pineapplewriter.yaml
